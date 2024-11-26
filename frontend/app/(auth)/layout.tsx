882b9f99d2416126d1bce211828a0101cb0a9c63
M. Yudistiandy Prabowo <andymyp1997@gmail.com>
2024-11-26 19:21:33 +0700

    FE: public and protected routes

---
+++ frontend/app/(auth)/layout.tsx
@@ -0,0 +1,16 @@
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export default async function AuthLayout({ children }: Props) {
+  const authSession = await auth();
+
+  if (authSession) {
+    return redirect("/");
+  }
+
+  return <main className="flex antialiased w-full h-screen">{children}</main>;
+}
---
+++ frontend/app/(dashboard)/layout.tsx
@@ -0,0 +1,16 @@
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export default async function DashboardLayout({ children }: Props) {
+  const authSession = await auth();
+
+  if (!authSession) {
+    return redirect("/sign-in");
+  }
+
+  return <main className="flex antialiased w-full h-screen">{children}</main>;
+}
