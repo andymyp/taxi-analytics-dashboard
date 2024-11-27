b55c78f5f36cf594df12181b836369211ef6016b
M. Yudistiandy Prabowo <andymyp1997@gmail.com>
2024-11-27 15:38:17 +0700

    FE: setup axios

---
+++ frontend/lib/axios.ts
@@ -0,0 +1,67 @@
+import _axios from "axios";
+import { store } from "@/redux";
+import { AuthAction } from "@/redux/auth-slice";
+import { signOut } from "next-auth/react";
+
+const axios = _axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+axios.interceptors.request.use(
+  async (config) => {
+    const token = store.getState().auth.token;
+
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+    const refreshToken = store.getState().auth.refreshToken;
+
+    if (
+      refreshToken &&
+      error.response?.status === 401 &&
+      !originalRequest._retry
+    ) {
+      originalRequest._retry = true;
+
+      try {
+        const refreshResponse = await _axios.get(
+          `${process.env.NEXT_PUBLIC_API_URL}/auth/refresh-token`,
+          {
+            headers: {
+              Authorization: `Bearer ${refreshToken}`,
+            },
+          }
+        );
+
+        const newToken = refreshResponse.data;
+        originalRequest.headers["Authorization"] = `Bearer ${newToken}`;
+        return axios(originalRequest);
+      } catch (refreshError) {
+        store.dispatch(AuthAction.signOut());
+        await signOut();
+        return Promise.reject(refreshError);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default axios;
